e424d29261f46904cc5e2fe4f173f3f69ddb1fd2
Baptiste P <baptiste.pauchet13@gmail.com>
2026-01-14 14:52:11 +0100

    Passage en fichier php

---
+++ composant/header.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="style/header.css">
+<header>
+    <img src="img/logo.png" alt="Logo">  
+    <nav>
+        <ul>
+            <li><a href="#home">Accueil</a></li>
+            <li><a href="#projects">Projets</a></li>
+            <li><a href="#skills">Compétences</a></li>
+            <li><a href="#experience">Expérience</a></li>
+            <li><a href="#education">Mon CV</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+</header>
